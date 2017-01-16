9beff47dbd439807f9f637ab6c08676c2bf96594
Alvin(Xinyao) Sun <xinyao1@ualberta.ca>
2017-01-16 03:23:52 -0700

    1 Share varibale

--- scripts/drnn_fusion_merge.py
+++ scripts/drnn_fusion_merge.py
@@ -3,7 +3,7 @@ import tensorflow.contrib.layers as tc
 import numpy as np
 import data_util
 
-IS_TRAINING = 0
+IS_TRAINING = 1
 START_LEARNING_RATE = 0.0015
 
 
@@ -17,12 +17,13 @@ class Config(object):
     rnn_layer_size = 5
     keep_prob_dense = 0.2
     keep_prob_rnn = 0.6
+    is_training = True
     start_learning_rate = START_LEARNING_RATE
 
 
 class TrainConfig(Config):
     batch_size = 1
-    time_steps = 500
+    time_steps = 3000
     input_size = 11
     output_size = 2
     rnn_layer_size = 3
@@ -34,9 +35,11 @@ class TrainConfig(Config):
 
 
 class TestConfig(TrainConfig):
+    batch_size = 1
     time_steps = 1
     keep_pro_dense = 1
     keep_pro_rnn = 1
+    is_training = False
 
 
 class DRNNFusion(object):
@@ -51,9 +54,16 @@ class DRNNFusion(object):
         self.start_learning_rate = config.start_learning_rate
         self.dense_size = config.dense_size
         self.rnn_layer_size = config.rnn_layer_size
+        self.is_training = config.is_training
 
         # Build the net
-        with tf.variable_scope("inputs"):
+        if self.is_training is True:
+            name_prefix = "train"
+        else:
+            name_prefix = "test"
+
+        print name_prefix
+        with tf.variable_scope(name_prefix + "_inputs"):
             self.xs_sensor_1 = tf.placeholder(tf.float32, [self.batch_size, self.time_steps, self.input_size],
                                               name='xs_sensor_1')
             self.xs_sensor_2 = tf.placeholder(tf.float32, [self.batch_size, self.time_steps, self.input_size],
@@ -63,21 +73,25 @@ class DRNNFusion(object):
         with tf.variable_scope("configure"):
             self.keep_prob_rnn_tf = tf.placeholder(tf.float32, name="keep_prob_rnn")
             self.keep_prob_dense_tf = tf.placeholder(tf.float32, name="keep_prob_rnn_dense_tf")
-            self.learning_rate = tf.placeholder(tf.float32, name="learning_rate")
-            tf.summary.scalar('lr', self.learning_rate)
-        with tf.name_scope('fc1'):
-            with tf.variable_scope("fc1_variable"):
+            if self.is_training is True:
+                self.learning_rate = tf.placeholder(tf.float32, name="learning_rate")
+            if self.is_training is True:
+                tf.summary.scalar('lr', self.learning_rate)
+        with tf.name_scope('input_fc'):
+            with tf.variable_scope("fc_variable"):
                 # sensor 1
                 fc1_inputs_sensor_1 = self.reshape_to_flat(self.xs_sensor_1, [-1, self.input_size])
                 fc1_inputs_sensor_2 = self.reshape_to_flat(self.xs_sensor_2, [-1, self.input_size])
 
-                fc1_sensor_1 = self.add_fc_layer(fc1_inputs_sensor_1, self.dense_size, None, self.keep_prob_dense_tf,
-                                                 name="fc1_s1_1")
+                fc1_sensor_1 = self.add_fc_layer(fc1_inputs_sensor_1, self.dense_size, "fc2_s1_1",
+                                                 self.keep_prob_dense_tf,
+                                                 act_fn=tf.nn.relu)
                 # fc1_sensor_1 = self.add_fc_layer(fc1_s1_1, self.rnn_cell_size, True, self.keep_prob_dense_tf,
                 #                                  name="fc1_s1_2")
 
-                fc1_sensor_2 = self.add_fc_layer(fc1_inputs_sensor_2, self.dense_size, None, self.keep_prob_dense_tf,
-                                                 name="fc1_s2_1")
+                fc1_sensor_2 = self.add_fc_layer(fc1_inputs_sensor_2, self.dense_size, "fc1_s2_1",
+                                                 self.keep_prob_dense_tf,
+                                                 )
                 # fc1_sensor_2 = self.add_fc_layer(fc1_s2_1, self.rnn_cell_size, True, self.keep_prob_dense_tf,
                 #                                  name="fc1_s2_2")
 
@@ -96,18 +110,21 @@ class DRNNFusion(object):
             merged_outputs = tf.concat(1, [self.reshape_to_flat(self.cell_outputs_sensor_1, [-1, self.rnn_cell_size]),
                                            self.reshape_to_flat(self.cell_outputs_sensor_2, [-1, self.rnn_cell_size])])
 
-        with tf.name_scope('output_fc2'):
-            with tf.variable_scope("fc2_variable") as scope:
-                self.fc2 = tf.nn.dropout(
-                    tc.layers.fully_connected(merged_outputs, self.dense_size, activation_fn=tf.nn.relu,
-                                              weights_initializer=tc.initializers.xavier_initializer(),
-                                              ), self.keep_prob_dense_tf)
+        with tf.name_scope('output_fc'):
+            with tf.variable_scope("fc_variable"):
+                self.fc2 = self.add_fc_layer(merged_outputs, self.dense_size, "fc_2_1", self.keep_prob_dense_tf,
+                                             tf.nn.relu)
+
                 # fc_2_2 = self.add_fc_layer(self.fc2, self.dense_size, True, self.keep_prob_dense_tf, "fc_2_2")
-                self.outputs = tf.nn.dropout(
-                    tc.layers.fully_connected(self.fc2, self.output_size, activation_fn=None,
-                                              weights_initializer=tc.initializers.xavier_initializer(),
-                                              ), self.keep_prob_dense_tf)
+                self.outputs = self.add_fc_layer(self.fc2, self.output_size, "fc_2_2", self.keep_prob_dense_tf,
+                                                 None)
+        with tf.name_scope("correct_pred"):
+            self.correct_pred = tf.arg_max(self.outputs, 1)
+        if self.is_training is False:
+            return
         with tf.name_scope('cost'):
+            if self.is_training is False:
+                tf.get_variable_scope().reuse_variables()
             losses = tf.nn.seq2seq.sequence_loss(
                 [tf.reshape(self.outputs, [-1, self.output_size], name="reshape_pred")],
                 [tf.reshape(self.ys, [-1, self.output_size], name="reshape_target")],
@@ -125,25 +142,26 @@ class DRNNFusion(object):
         with tf.name_scope("train"):
             self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost)
 
-        with tf.name_scope("correct_pred"):
-            self.correct_pred = tf.arg_max(self.outputs, 1)
-
     def reshape_to_flat(self, inputs, shape):
         return tf.reshape(inputs, shape=shape, name="2_2D")
 
     def reshape_to_three_d(self, inputs, shape):
         return tf.reshape(inputs, shape=shape, name="2_3D")
 
-    def add_fc_layer(self, inputs, outputs, is_drop, keep_prob, name):
+    def add_fc_layer(self, inputs, outputs, name, keep_prob=None, act_fn=None):
         with tf.variable_scope(name) as scope:
-            fc = tc.layers.fully_connected(inputs, outputs, activation_fn=tf.nn.relu,
+            if self.is_training is False:
+                tf.get_variable_scope().reuse_variables()
+            fc = tc.layers.fully_connected(inputs, outputs, activation_fn=act_fn,
                                            weights_initializer=tc.initializers.xavier_initializer(),
-                                           scope=scope)
-            if is_drop:
+                                           scope=scope, reuse=not self.is_training)
+            if self.is_training and keep_prob is not None:
                 fc = tf.nn.dropout(fc, keep_prob)
         return fc
 
     def add_rnn_cell(self, inputs, scope):
+        if self.is_training is False:
+            tf.get_variable_scope().reuse_variables()
         lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(self.rnn_cell_size, forget_bias=1.0, state_is_tuple=True)
         lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=self.keep_prob_rnn_tf)
         stacked_lstm = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * self.rnn_layer_size)
@@ -156,7 +174,6 @@ class DRNNFusion(object):
 
 def eval_accuracy(sess, model, data_set_instance, val_init_state, mode=1):
     predict = []
-    lr = model.start_learning_rate
     batch_x, _, _ = data_set_instance.get_batch()
     if mode == 2:
         feed_dict = {
@@ -166,7 +183,6 @@ def eval_accuracy(sess, model, data_set_instance, val_init_state, mode=1):
             model.keep_prob_dense_tf: 1,
             model.cell_init_state_sensor_1: val_init_state,
             model.cell_init_state_sensor_2: val_init_state,
-            model.learning_rate: lr
         }
         pred, val_state_1, val_state_2 = sess.run(
             [model.correct_pred, model.cell_final_state_sensor_1,
@@ -183,7 +199,6 @@ def eval_accuracy(sess, model, data_set_instance, val_init_state, mode=1):
                 model.keep_prob_dense_tf: 1,
                 model.cell_init_state_sensor_1: val_init_state,
                 model.cell_init_state_sensor_2: val_init_state,
-                model.learning_rate: lr
             }
         else:
             feed_dict = {
@@ -193,7 +208,6 @@ def eval_accuracy(sess, model, data_set_instance, val_init_state, mode=1):
                 model.keep_prob_dense_tf: 1,
                 model.cell_init_state_sensor_1: val_state_1,
                 model.cell_init_state_sensor_2: val_state_2,
-                model.learning_rate: lr
             }
         pred, val_state_1, val_state_2 = sess.run(
             [model.correct_pred, model.cell_final_state_sensor_1,
@@ -268,7 +282,7 @@ def run_train(sess, model, train_data):
         epoch_cost += cost
         epoch_count += 1
         if epoch % 1 == 0 and is_reset == 1:
-            eval_accuracy(sess, model, test_data_set, train_init_state, 2)
+            eval_accuracy(sess, drnn_fusion_model_evl, test_data_set, train_init_state, 1)
 
 
 def run_eval(sess, model, test_data, train_init_state=None):
@@ -280,29 +294,35 @@ def run_eval(sess, model, test_data, train_init_state=None):
 
 
 if __name__ == "__main__":
-    if IS_TRAINING == 1:
-        train_config = TrainConfig()
-        drnn_fusion_model = DRNNFusion(train_config)
-    else:
-        test_config = TestConfig()
-        drnn_fusion_model = DRNNFusion(test_config)
-    session = tf.Session()
+    with tf.variable_scope('model') as scope:
+        if IS_TRAINING == 1:
+            train_config = TrainConfig()
+            drnn_fusion_model = DRNNFusion(train_config)
+            drnn_fusion_model_evl = DRNNFusion(TestConfig())
+            _, test_data_set = data_util.prepare_data(drnn_fusion_model_evl.batch_size,
+                                                      drnn_fusion_model_evl.time_steps)
+            train_data_set, _ = data_util.prepare_data(drnn_fusion_model.batch_size,
+                                                       drnn_fusion_model.time_steps)
+        else:
+            test_config = TestConfig()
+            drnn_fusion_model = DRNNFusion(test_config)
+            train_data_set, test_data_set = data_util.prepare_data(drnn_fusion_model.batch_size,
+                                                                   drnn_fusion_model.time_steps)
+        session = tf.Session()
 
-    saver = tf.train.Saver()
+        saver = tf.train.Saver()
 
-    train_data_set, test_data_set = data_util.prepare_data(drnn_fusion_model.batch_size,
-                                                           drnn_fusion_model.time_steps)
-    init = tf.global_variables_initializer()
-    session.run(init)
+        init = tf.global_variables_initializer()
+        session.run(init)
+        print (drnn_fusion_model.time_steps)
+        ckpt = tf.train.get_checkpoint_state('tmp/')
 
-    ckpt = tf.train.get_checkpoint_state('tmp/')
-
-    if ckpt and ckpt.model_checkpoint_path:
-        saver.restore(session, ckpt.model_checkpoint_path)
-        print('Checkpoint found')
-    else:
-        print('No checkpoint found')
-    if IS_TRAINING == 1:
-        run_train(session, drnn_fusion_model, train_data_set)
-    else:
-        run_eval(session, drnn_fusion_model, test_data_set)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(session, ckpt.model_checkpoint_path)
+            print('Checkpoint found')
+        else:
+            print('No checkpoint found')
+        if IS_TRAINING == 1:
+            run_train(session, drnn_fusion_model, train_data_set)
+        else:
+            run_eval(session, drnn_fusion_model, test_data_set)
